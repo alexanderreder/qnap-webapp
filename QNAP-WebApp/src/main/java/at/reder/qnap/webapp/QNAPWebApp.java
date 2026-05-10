c02ef74d824d9ea4f2ab228d7a07267155dfba79
Alexander Reder <alexander.reder@gmail.com>
2026-05-10 20:55:42 +0200

    * ports changed

--- QNAP-WebApp/src/main/java/at/reder/qnap/webapp/QNAPWebApp.java
+++ QNAP-WebApp/src/main/java/at/reder/qnap/webapp/QNAPWebApp.java
@@ -13,17 +13,17 @@ public class QNAPWebApp {
     public static void main(String[] args) {
         // Start Car Statistics on port 8081
         SpringApplication carApp = new SpringApplication(at.reder.carstatistics.Carstatistics.class);
-        carApp.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
+        carApp.setDefaultProperties(Collections.singletonMap("server.port", "8082"));
         carApp.run(args);
 
         // Start Teaching on port 8082
         SpringApplication teachingApp = new SpringApplication(at.reder.teaching.webapp.WebApp.class);
-        teachingApp.setDefaultProperties(Collections.singletonMap("server.port", "8082"));
+        teachingApp.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
         teachingApp.run(args);
 
         // Start QNAP Hub on port 8080
         SpringApplication hubApp = new SpringApplication(QNAPWebApp.class);
-        hubApp.setDefaultProperties(Collections.singletonMap("server.port", "8080"));
+        hubApp.setDefaultProperties(Collections.singletonMap("server.port", "8081"));
         hubApp.run(args);
     }
 }
